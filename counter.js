0da3fbaa0a97f68f0e6205d865bd8d29121df3bb
core-90 <spe82c@gmail.com>
2022-09-12 16:19:28 -0500

    Implemented the use of the event paremeter as it is done in the book

--- counter.js
+++ counter.js
@@ -2,20 +2,41 @@
 const Counter = {
     data() {
         return {
-            count: parseInt(this.start),
+            count : 0,
+            message : "",
         }
     },
-    template : ` <button @click="this.count++">Increment counter by count++</button>
-                    &nbsp;&nbsp; count = {{count}} <br><br>
-                <button @click="incr()">Increment counter by incr()</button>
-                    &nbsp;&nbsp; count = {{count}} <br><br>`,
+    template : ` 
+        <!--The event parameter is passed valid processing function-->
+        count (less than 100): <input type="text"
+        :value="count" @blur="valid($event)" @keydown="verif($event)"/>
+        &nbsp;&nbsp; count = {{count}}
+        <br><br>
+        <span>{{message}}</span>`,
     methods : {
-        incr(){
-            this.count++;
+        valid(event){
+            this.message = "";
+            if(event.target.value < 100){
+                this.count = event.target.value;
+            }else{
+                this.message = "Error: count must be less than 100";
+            }
+        },
+        verif(event){
+            console.log("value of the key pressed: ", event.key);// display in the console 
+                                                                // the value of the key 
+                                                                // pressed
+            if (event.key != "Backspace" && event.key != "Delete" 
+            &&
+                event.key != "ArrowLeft" && event.key != "ArrowRight" &&
+                event.key != "Tab"){
+                    //forbid the key if it is not numeric
+                    if(event.key < "0" || event.key > "9"){
+                        event.preventDefault();
+                    }
+                }
         }
     },
-    props : [
-        "start"
-    ]
+    
 }
 export default Counter;

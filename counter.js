5ee34bbe9362218df7b75155b25f157804229c03
YeiH <yeisson.garcia@lafargeholcim.com>
2022-09-11 23:53:26 -0500

    implemented an example of v-model use

--- counter.js
+++ counter.js
@@ -4,26 +4,16 @@ const Counter = {
         is not inserted (if the condition is false).*/
     data() {
         return {
-            counts: ["Counter 1", "Counter 2", "Counter 3",
-                "Counter 4", "Counter 5"],
-            //Array use to show v-for index use
-            indexedCounts: ["iCounter 1", "iCounter 2", "iCounter 3",
-                "iCounter 4", "iCounter 5"],
+            count : 10,
         }
     },
     template: `
-        <!--Displaying counters as a list-->
-        <ul>
-            <li v-for="count in counts">
-                <span>{{count}}</span>
-            </li>
-        </ul>
-        <!--Displaying counters and their index (counter.js file)-->
-        <ul>
-            <li v-for="(count, index) in indexedCounts">
-                <span>Index {{index}} : {{count}}</span>
-            </li>
-        </ul>`,
+        Without v-model:
+            <input type="text" :value="count" /> &nbsp;&nbsp;
+            count = {{count}} <br><br>
+        With v-model:
+            <input type="text" v-model="count" /> &nbsp;&nbsp;
+            count = {{count}} `,
 
 }
 
